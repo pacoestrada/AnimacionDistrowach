b042746551219c16904de57156a0c636e50ceaaa
pacoestrada <pacoestrada@gmail.com>
2023-08-08 12:02:23 +0200

    cambio de visualizacion

--- distros_evolucion.py
+++ distros_evolucion.py
@@ -13,24 +13,26 @@ def update(num):
     ax.clear()
     year = df['Año'].iloc[num]
     data = df.iloc[num, 1:].reset_index()
-    data.columns = ["Rank", "Distro"]
-    
-    # Ordenamos por posición y reseteamos el índice
+    data.columns = ["Distro", "Rank"]
     data = data.sort_values("Rank").reset_index(drop=True)
     
-    # Usamos barras verticales
-    ax.bar(data.index, range(1, len(data)+1), color=colors, width=0.8)
+    # Usamos barras horizontales
+    bars = ax.barh(data.index, data["Rank"], color=colors, height=0.8)
     
-    # Añadir el nombre de las distribuciones como etiquetas en el eje x
-    ax.set_xticks(data.index)
-    ax.set_xticklabels(data["Distro"], rotation=45, ha='right')
+    # Añadir el nombre de las distribuciones dentro de las barras
+    for bar, distro in zip(bars, data["Distro"]):
+        ax.text(bar.get_width() - 0.5, bar.get_y() + bar.get_height()/2, 
+                distro, va='center', ha='right', color='white', fontsize=10)
+    
+    # Ajustamos el rango del eje y y x
+    ax.set_ylim(-1, len(df.columns)-1)
+    ax.set_xlim(10, 0)  # Las posiciones son del 1 al 10, invertimos para que el 1 esté a la izquierda
     
     ax.set_title(f'Distribuciones de Linux en el año {year}')
-    ax.set_ylabel("Ranking")
-    
-    # Ajustamos el rango del eje y
-    ax.set_ylim(0, len(df.columns))
-    
+    ax.set_xlabel("Ranking")
+    ax.set_yticks(range(10))
+    ax.set_yticklabels([f"{i+1}º" for i in range(10)])
+
     plt.tight_layout()
 
 # La duración del intervalo determina la velocidad de la animación: un valor más alto la ralentizará
