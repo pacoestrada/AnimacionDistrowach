36b77303f5f10bdbc89b7ac11bc6c55c1261ca65
pacoestrada <pacoestrada@gmail.com>
2023-08-08 18:50:14 +0200

    ajustando nombres y barras

--- distros_evolucion.py
+++ distros_evolucion.py
@@ -8,8 +8,12 @@ warnings.filterwarnings("ignore", category=UserWarning, module="bar_chart_race")
 # Leer el archivo CSV con los datos transformados
 df_transformed = pd.read_csv('ranking_distribuciones_transformado.csv', index_col=0)
 
+# Para mostrar el número de ranking en el eje y en lugar de los nombres de las distribuciones, 
+# transformamos los datos para que las distribuciones sean las columnas y los años las filas
+df_bcr = df_transformed.T
+
 # Crear el barchart race
-bcr.bar_chart_race(df_transformed, 
+bcr.bar_chart_race(df_bcr, 
                    title='Ranking de Distribuciones por Año', 
                    n_bars=10, 
                    sort='desc',
@@ -17,7 +21,11 @@ bcr.bar_chart_race(df_transformed,
                    cmap='dark24',
                    bar_label_size=7,
                    tick_label_size=7,
-                   period_length=1000,  # 1000 milisegundos = 1 segundo por año, pero puedes ajustarlo
+                   period_length=100,  # 1000 milisegundos = 1 segundo por año, pero puedes ajustarlo
                    filename='barchart_race.mp4',  # Esto guardará el video en formato .mp4
-                   filter_column_colors=True)  
-
+                   filter_column_colors=True,
+                   bar_label_position='inside',
+                   period_label={'x': .98, 'y': .3, 'ha': 'right', 'va': 'center'},
+                   period_summary_func=lambda v, r: {'x': .98, 'y': .18, 
+                                                    's': f'Year: {r.index.max()}', 
+                                                    'ha': 'right', 'size': 8, 'family': 'Courier New'})
