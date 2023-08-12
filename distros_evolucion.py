6d2ab7f85a7be4622d4ef31fe3320058ee44a15e
pacoestrada <pacoestrada@gmail.com>
2023-08-12 18:42:47 +0200

    Cambiando la función

--- distros_evolucion.py
+++ distros_evolucion.py
@@ -1,50 +1,62 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-import matplotlib.patches as mpatches
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, PillowWriter
 
-# Definimos colores para las distribuciones
+# Cargar el CSV
+df = pd.read_csv("DistrosTiempo.csv")
+
+# Transformar los datos
+df_melted = df.melt(id_vars=['Año'], 
+                    value_vars=df.columns[1:], 
+                    var_name='Posición', 
+                    value_name='Distribución')
+df_melted['Posición'] = df_melted['Posición'].str.extract('(\d+)').astype(int)
+df_melted = df_melted.sort_values(by=['Año', 'Posición'])
+
+# Definir los colores
 colors = {
-    'Slackware': '#000000',  # Negro
-    'Debian': '#D70A53',     # Rojo Debian
-    'Red Hat': '#CC0000',    # Rojo Red Hat
-    'Mandrake': '#2D6BA1',   # Azul
-    'SuSE': '#0A74DA',       # Azul SuSE
-    'Gentoo': '#54487A',     # Morado Gentoo
-    'Ubuntu': '#E95420',     # Naranja Ubuntu
-    'LFS': '#C7A252',        # Dorado
-    'FreeBSD': '#AB2D2D',    # Rojo FreeBSD
-    'NetBSD': '#F57900',     # Naranja
-    'Fedora': '#294172'      # Azul Fedora
+    'Slackware': '#000000',
+    'Debian': '#D70A53',
+    'Red Hat': '#CC0000',
+    'Mandrake': '#2D6BA1',
+    'SuSE': '#0A74DA',
+    'Gentoo': '#54487A',
+    'Ubuntu': '#E95420',
+    'LFS': '#C7A252',
+    'FreeBSD': '#AB2D2D',
+    'NetBSD': '#F57900',
+    'Fedora': '#294172'
 }
 
-# Preparar la figura y los ejes
-fig, ax = plt.subplots(figsize=(12, 8))
-ax.set_xlim(10, 1)  # Invertimos el eje X para que 1 esté a la derecha y 10 a la izquierda
-ax.set_ylim(10.5, 0.5)  # Posición de las barras
-ax.set_title('Ranking de Distribuciones Linux por Año', fontsize=20)
-
-# Función para actualizar la animación
+# Función de actualización para la animación
 def update(year):
-    ax.clear()  # Limpiamos el lienzo para cada frame
+    ax.clear()
     data_year = df_melted[df_melted['Año'] == year].copy()
     data_year.set_index('Distribución', inplace=True)
-    
-    # Dibujar las barras
     for distro, color in colors.items():
         if distro in data_year.index:
-            position = data_year.loc[distro, 'Posición']
+            position_value = data_year.loc[distro, 'Posición']
+            position = position_value.iloc[0] if hasattr(position_value, 'iloc') else position_value            
             ax.barh(position, position, color=color, edgecolor='white', height=0.8)
             ax.text(5, position, distro, ha='center', va='center', fontsize=12, color='white')
-    
-    # Configuraciones adicionales para el gráfico
     ax.set_xlim(10, 1)
     ax.set_ylim(10.5, 0.5)
     ax.set_title('Ranking de Distribuciones Linux por Año', fontsize=20)
     ax.text(5, 11.5, f'Año: {year}', ha='center', fontsize=15, fontweight='bold')
-    ax.axis('off')  # Ocultamos los ejes
+    ax.axis('off')
+
+# Preparar la figura y ejes
+fig, ax = plt.subplots(figsize=(12, 8))
 
 # Crear la animación
 ani = FuncAnimation(fig, update, frames=df['Año'].unique(), repeat=False, interval=2000)
 
+# Guardar la animación (opcional)
+writer = PillowWriter(fps=0.5)
+ani.save("barchart_race.gif", writer=writer)
+
 plt.show()
+plt.pause(0.001)
+input("Press [enter] to continue.")
