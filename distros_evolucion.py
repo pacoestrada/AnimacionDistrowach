d4c37b9f644110868c8fb1d76e7482efb4f2afdb
pacoestrada <pacoestrada@gmail.com>
2023-08-12 19:07:30 +0200

    Actualizando diccionario de colores

--- distros_evolucion.py
+++ distros_evolucion.py
@@ -1,5 +1,3 @@
-import matplotlib
-matplotlib.use('TkAgg')
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
@@ -34,17 +32,19 @@ colors = {
 def update(year):
     ax.clear()
     data_year = df_melted[df_melted['Año'] == year].copy()
-    data_year.set_index('Distribución', inplace=True)
     for distro, color in colors.items():
-        if distro in data_year.index:
-            position_value = data_year.loc[distro, 'Posición']
-            position = position_value.iloc[0] if hasattr(position_value, 'iloc') else position_value            
-            ax.barh(position, 10 - position + 1, color=color, edgecolor='white', height=0.8)
-            ax.text(10.5 - position, position, distro, ha='left', va='center', fontsize=12, color='black')
-    ax.set_xlim(0, 10)
+        position_data = data_year[data_year['Distribución'] == distro]
+        if not position_data.empty:
+            position = position_data['Posición'].values[0]
+            ax.barh(position, position, color=color, edgecolor='white', height=0.8)
+            if position > 4:
+                ax.text(position-0.5, position, distro, ha='right', va='center', fontsize=10, color='white')
+            else:
+                ax.text(position+0.5, position, distro, ha='left', va='center', fontsize=10, color='black')
+    ax.set_xlim(10, 1)
     ax.set_ylim(10.5, 0.5)
     ax.set_title('Ranking de Distribuciones Linux por Año', fontsize=20)
-    ax.text(5, 11.5, f'Año: {year}', ha='center', fontsize=15, fontweight='bold')
+    ax.text(5.5, 11, f'Año: {year}', ha='center', fontsize=15, fontweight='bold')
     ax.axis('off')
 
 # Preparar la figura y ejes
@@ -54,9 +54,7 @@ fig, ax = plt.subplots(figsize=(12, 8))
 ani = FuncAnimation(fig, update, frames=df['Año'].unique(), repeat=False, interval=1000)
 
 # Guardar la animación (opcional)
-writer = PillowWriter(fps=1)                      
+writer = PillowWriter(fps=1)
 ani.save("barchart_race.gif", writer=writer)
 
 plt.show()
-plt.pause(0.001)
-input("Press [enter] to continue.")
